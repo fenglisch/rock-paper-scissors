8fd00c9d46ee76b74519006445e731b4392c61d8
fenglisch <fenglisch@web.de>
2023-01-09 16:57:24 +0100

    Remove prompts, alerts and console dialogs; add basic GUI

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
 
 let userScore = 0;
 let computerScore = 0;
+let userChoice;
+let computerChoice;
 
-for (let i = 1; i <= 5; i++) {
-    console.log("Round " + i)
-    let userChoice;
-    let computerChoice;
-
-    function getComputerChoice() {
-        let computerNumber = Math.floor(Math.random()*100) + 1;
-        console.log(computerNumber);
-        if (computerNumber <= 33) {
-            computerChoice = "rock";
-            console.log("computerChoice = " + computerChoice);
-            return computerChoice
-        }
-        else if (computerNumber <= 66) {
-            computerChoice = "paper";
-            console.log("computerChoice = " + computerChoice);
-            return computerChoice
-        }
-        else if (computerNumber <= 99) {
-            computerChoice = "scissors";
-            console.log("computerChoice = " + computerChoice);
-            return computerChoice
-        }
-        else {
-            getComputerChoice()
-        }
+function getComputerChoice() {
+    let computerNumber = Math.floor(Math.random()*100) + 1;
+    console.log(computerNumber);
+    if (computerNumber <= 33) {
+        computerChoice = "rock";
+        console.log("computerChoice = " + computerChoice);
+        return computerChoice
     }
+    else if (computerNumber <= 66) {
+        computerChoice = "paper";
+        console.log("computerChoice = " + computerChoice);
+        return computerChoice
+    }
+    else if (computerNumber <= 99) {
+        computerChoice = "scissors";
+        console.log("computerChoice = " + computerChoice);
+        return computerChoice
+    }
+    else {
+        getComputerChoice()
+    }
+}
 
+
+
+
+let btns = document.querySelectorAll(".btn");
+let feedback = document.querySelector(".feedback")
+let finalResult = document.querySelector(".final-result")
+let usBoard = document.querySelector(".user-score-board")
+let csBoard = document.querySelector(".computer-score-board")
+ 
+btns.forEach((button) => {
+     button.addEventListener("click", function(e) {
+        if (userScore < 5 && computerScore < 5) {
+            computerChoice = getComputerChoice();
+            if (e.target.id == "btn-rock") userChoice = "rock";
+            else if (e.target.id == "btn-paper") userChoice = "paper";
+            else if (e.target.id == "btn-scissors") userChoice = "scissors";
+            console.log(userChoice)
+            compareChoices(computerChoice, userChoice)
+            usBoard.textContent = "Your score: " + userScore
+            csBoard.textContent = "Computer's score: " + computerScore
+
+            if (userScore >= 5 || computerScore >= 5) {
+                console.log("Final computerScore = " + computerScore)
+                console.log("Final userScore = " + userScore)
+                if (userScore > computerScore) {
+                    finalResult.textContent = "Your score is " + userScore + " and the computer's score is " + computerScore + ". Congratulations, you won! Refresh the page to play again."
+                }
+                else if (userScore < computerScore) {
+                    finalResult.textContent = "Your score is " + userScore + " and the computer's score is " + computerScore + ". You lost! Refresh the page to play again."
+                }
+        }
+        }
+})});
+
+
+/*
     function getUserChoice() {
         let userAnswer = prompt("Round " + i + " of 5. What do you choose?","Type 'Rock', 'Paper' or 'Scissors'").toLowerCase()
         console.log("userAnswer = " + userAnswer)
@@ -44,42 +77,30 @@ for (let i = 1; i <= 5; i++) {
             getUserChoice()
         }
         return userChoice;
-    }
+    };
+*/
+    
 
-    computerChoice = getComputerChoice()
-    userChoice = getUserChoice()
 
+
+function compareChoices(computerChoice, userChoice) {
 
     if (userChoice == "rock" && computerChoice == "scissors" || userChoice == "paper" && computerChoice == "rock" || userChoice == "scissors" && computerChoice == "paper") {
-        alert("You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". You win!");
         ++userScore;
+        feedback.textContent = "You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". You win!"
         console.log("userScore = " + userScore);
         console.log("computerScore = " + computerScore)
     }
     else if (userChoice == "rock" && computerChoice == "paper" || userChoice == "paper" && computerChoice == "scissors" || userChoice == "scissors" && computerChoice == "rock") {
-        alert("You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". You lose.");
         ++computerScore;
+        feedback.textContent = "You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". You lose.";
         console.log("userScore = " + userScore);
         console.log("computerScore = " + computerScore);
 
     }
     else {
-        alert("You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". It's a draw.")
+        feedback.textContent = "You have chosen " + userChoice + " and the computer has chosen " + computerChoice + ". It's a draw";
         console.log("userScore = " + userScore);
         console.log("computerScore = " + computerScore);
     }
-}
-
-console.log("Final computerScore = " + computerScore)
-console.log("Final userScore = " + userScore)
-
-if (userScore > computerScore) {
-    alert("Your score is " + userScore + " and the computer's score is " + computerScore + ". Congratulations, you won! Refresh the page to play again.")
-}
-
-else if (userScore < computerScore) {
-    alert("Your score is " + userScore + " and the computer's score is " + computerScore + ". You lost! Refresh the page to play again.")
-}
-else {
-    alert("You and the computer both scored " + userScore + " point(s). It's a draw. Refresh the page to play again.")
 }
